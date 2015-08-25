49662a6f78452e7886ea8443d18cb44275141edd
Chris Schmidt <chris@superbilk.org>
2015-08-25 22:05:28 +0200

    index for signup

---
+++ db/migrate/20150825200400_add_index_for_omniauth_to_users.rb
@@ -0,0 +1,5 @@
+class AddIndexForOmniauthToUsers < ActiveRecord::Migration
+  def change
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825193005) do
+ActiveRecord::Schema.define(version: 20150825200400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150825193005) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
   add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
   add_index "users", ["username", "provider"], name: "index_users_on_username_and_provider", unique: true, using: :btree
