5a823eedb3685bfd66ebe75b9a79d3ed6cae411c
Chris Schmidt <chris@superbilk.org>
2015-09-03 22:15:14 +0200

    remove examples

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @user = current_user
-    begin
-      @my_timeline = current_user.twitter.home_timeline(count: 5)
-    rescue Twitter::Error::TooManyRequests => error
-      @my_timeline = []
-    end
+    # @user = current_user
+    # begin
+    #   @my_timeline = current_user.twitter.home_timeline(count: 5)
+    # rescue Twitter::Error::TooManyRequests => error
+    #   @my_timeline = []
+    # end
   end
 end
